b4ba83a4ad8d91a52eef35bc8fcec227a3b19913
Daan Zeeuwe <zeeuwed001@gmail.com>
2020-03-09 09:42:50 +0100

    Finalizing visualization and fixing animation bugs
    Integrated new visualizations for candidates, background and labels. Together with fixing the child animation bug by moving it outside of the processing thread.

--- src/view/QMovieLabel.py
+++ src/view/QMovieLabel.py
@@ -12,17 +12,24 @@ class QMovieLabel(QLabel):
 
     def __init__(self, fileName, parent=None):
         super(QMovieLabel, self).__init__(parent)
-        self.initialize(fileName)
 
         self.movie = None
+        self._movieHeight = 0
+        self._movieWidth = 0
+
+        self.path = ""
+
+        self.initialize(fileName)
 
     def initialize(self, fileName):
-        self.movie = QMovie(fileName)
-        self.setMovie(self.movie)
-        self.movie.start()
+        if self.path is not fileName:
+            self.path = fileName
+            self.movie = QMovie(fileName)
+            self.setMovie(self.movie)
+            self.movie.start()
 
     def setMovie(self, movie):
         super(QMovieLabel, self).setMovie(movie)
-        s=movie.currentImage().size()
+        s = movie.currentImage().size()
         self._movieWidth = s.width()
         self._movieHeight = s.height()
